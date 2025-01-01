5ad7e00e66cc2d3ee09c9cc53eba48e4045de8a2
sanjaygithub7 <sanjaykumar712121@gmail.com>
2025-01-01 13:59:17 +0530

    refactor: removed unwanted comments in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,34 @@ const express = require('express');
 const connectdatabase = require('./config/connectDB');
 const dotenv = require('dotenv');
 const path = require('path');
+const PORT =  3000;
 
 const app = express();
 
 // Load environment variables
 dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
 
-// Database connection
+//DB connection
 connectdatabase();
 
-// Middleware
+
 app.use(express.json());
 
-// Import routes
+// Importing routes
 const userRoutes = require('./routes/userroute');
 const expenseRoutes = require('./routes/expenseroute');
 
-// Mount routes
+
 app.use('/api/users', userRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-// Fallback for undefined routes
+// alternative for not available routes
 app.use((req, res) => {
     res.status(404).json({ message: 'Route not found' });
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
